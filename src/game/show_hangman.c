843ba6f1d9c07bbd12d179689025368fcc458f62
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-03-25 20:26:59 +0100

    Refractor undefined behavior

--- src/game/show_hangman.c
+++ src/game/show_hangman.c
@@ -1,10 +1,11 @@
 #include "../../includes/game.h"
 
 void show_hangman(int bad_guess_count){
+	char temp_path[HANGMAN_FILENAME_MAX_LENGTH] = {0};
 	char path[HANGMAN_FILENAME_MAX_LENGTH] = {0};
-	sprintf(path, "%s", HANGMAN_PATH);
-	sprintf(path + strlen(path), "%s", HANGMAN_FILENAME_FORMAT);
-	sprintf(path, path, bad_guess_count);
+
+	sprintf(temp_path, "%s%s", HANGMAN_PATH, HANGMAN_FILENAME_FORMAT);
+	sprintf(path, temp_path, bad_guess_count);
 	FILE * fileptr = fopen(path, "r");
 	if (fileptr == NULL){
 		printf("Something went wrong when opening: \"%s\"\n", path);
